8f8a8c61d1c3497629b7254d4bfc1a08668fd540
Peter <dchen@albany.edu>
2017-04-15 15:16:45 -0400

    Add files via upload

---
+++ Technical Interview/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(nums):
+	n = len(nums)
+	for i in range(n - 1, 0, -1):
+		for j in range(0, i, 1):
+			if nums[j] > nums[j + 1]:
+				temp = nums[j]
+				nums[j] = nums[j + 1]
+				nums[j + 1] = temp
+
+
+nums = [3,5,6,1,90,3,2,1,5,6]
+bubble_sort(nums)
+print(nums)
