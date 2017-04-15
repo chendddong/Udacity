42b8125664558f99a0310901d0402595a23dadc4
Peter <dchen@albany.edu>
2017-04-15 14:36:47 -0400

    Add files via upload

---
+++ Technical Interview/fibonacci.py
@@ -0,0 +1,25 @@
+"""Implement a function recursivly to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions."""
+
+# Isn't that elegant? We took a short function and managed to write it in even fewer lines! 
+# Apparently Google thinks recursion is pretty cool too if you search for "recursion" in Google's search engine, the first thing that pops up is "Did you mean: recursion". Don't get stuck in an infinite loop continuously searching "recursion"!
+# You may have noticed that this solution will compute the values of some inputs more than once. For example get_fib(4) calls get_fib(3) and get_fib(2), get_fib(3) calls get_fib(2) and get_fib(1) etc. The number of recursive calls grows exponentially with n.
+# In practice if we were to use recursion to solve this problem we should use a hash table to store and fetch previously calculated results. This will increase the space needed but will drastically improve the runtime efficiency.
+
+
+def get_fib(position):
+	if position == 0:
+		return 0
+	if position == 1:
+		return 1
+
+	if position >= 2:
+		return get_fib(position - 1) + get_fib(position - 2)
+    
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
